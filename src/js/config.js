cf70cbc947dda0f3377af949bbf7783954da788b
MayureshKhole <mbkhole@gmail.com>
2022-06-15 17:51:25 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'be39eafe-4152-412d-b902-92215ae3e9f5';
+export const MODAL_CLOSE_SEC = 2.5;
